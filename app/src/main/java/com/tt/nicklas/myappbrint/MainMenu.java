e50f5c9b46eda0276fe19e6b84e3924716dc431d
futte111 <ntthyssen@gmail.com>
2018-09-17 17:06:21 +0200

    added a svg image that can change color on button click

--- app/src/main/java/com/tt/nicklas/myappbrint/MainMenu.java
+++ app/src/main/java/com/tt/nicklas/myappbrint/MainMenu.java
@@ -1,22 +1,45 @@
 package com.tt.nicklas.myappbrint;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainMenu extends AppCompatActivity {
-
+    boolean changeTheme = true;
+    Drawable drawable;
+    ContextThemeWrapper wrapper1;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        wrapper1 = new ContextThemeWrapper(this, R.style.default1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Button b = (Button) findViewById(R.id.button);
         TextView t = (TextView) findViewById(R.id.text1);
+
         b.setOnClickListener((v) -> {
-            t.setText("this is new text");
+            if(changeTheme){
+                wrapper1 = new ContextThemeWrapper(this, R.style.Theme1);
+                drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_hangman, wrapper1.getTheme());
+                imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageDrawable(drawable);
+                changeTheme = false;
+            }else{
+                t.setText("This got creepy!");
+                wrapper1 = new ContextThemeWrapper(this, R.style.default1);
+                drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_hangman, wrapper1.getTheme());
+                imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageDrawable(drawable);
+                changeTheme = true;
+            }
+
         } );
 
     }
