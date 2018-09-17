22ac0cc37806fa2998eb3f11bd85c38f9e085e09
futte111 <ntthyssen@gmail.com>
2018-09-17 15:28:38 +0200

    Button on click change text added

--- app/src/main/java/com/tt/nicklas/myappbrint/MainMenu.java
+++ app/src/main/java/com/tt/nicklas/myappbrint/MainMenu.java
@@ -2,6 +2,10 @@ package com.tt.nicklas.myappbrint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button b = (Button) findViewById(R.id.button);
+        TextView t = (TextView) findViewById(R.id.text1);
+        b.setOnClickListener((v) -> {
+            t.setText("this is new text");
+        } );
+
     }
+
+
 }
